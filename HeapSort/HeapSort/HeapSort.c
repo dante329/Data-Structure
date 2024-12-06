98e59b3252a6f3112ff60deb68c77391323218c7
Dante <2024176553@qq.com>
2024-12-07 00:04:29 +0800

    直接选择排序和冒泡排序

--- HeapSort/HeapSort/HeapSort.c
+++ HeapSort/HeapSort/HeapSort.c
@@ -2,61 +2,78 @@
 
 #include <stdio.h>
 
-void Swap(int* p1, int* p2)
+/*
+void Swap(int* p1, int* p2)//当最小值（mini）和当前起始位置（start）的索引相同时，Swap(&a[start], &a[mini]) 实际上等价于 Swap(&a[start], &a[start])，即 p1 == p2。在这种情况下，异或操作会将值清零
 {
+	if (p1 == p2) return;//如果想要继续使用异或交换，需要增加安全检查以避免问题 避免 p1 和 p2 指向相同地址
 	*p1 = *p1 ^ *p2;
 	*p2 = *p1 ^ *p2;
-	*p1 ^= *p2;
-}
+	*p1 = *p1 ^ *p2;
+}//虽然异或操作在逻辑上可以交换两个值，但它的实现依赖于对数据位的逐位操作。如果程序的指针操作或数据一致性稍有问题（例如传递了相同指针），就会触发这种错误。相比之下，临时变量的交换更棒，不会受到这种影响。
+*/
 
-void AdjustDown(int* a, int n, int root)
+
+//最好还是用临时变量交换
+void Swap(int* x, int* y) 
 {
-	int parent = root;
-	int child = parent * 2 + 1;//先默认child是左孩子
-	while (child < n)//child增长的最快，所以限制child作为条件，数组的大小为n，数组的最后一个索引是n-1，不能越界所以child < n
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+
+//最垃圾的直接选择排序
+void SelectSort(int* a, int n)
+{	
+	int start = 0, end = n-1;
+	while (start < end)
 	{
-		if (child + 1 < n && a[child + 1] < a[child])//大顶堆改> //child + 1 < n 完全二叉树有可能没有右孩子，判断右孩子的下标是否超过了最大索引，超过了就证明该树没有右孩子
+		int mini = start, maxi = start;
+		for (int i = start; i <= end; i++)
 		{
-			child += 1;//如果a[child + 1]更小/更大，将child + 1赋给child
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
 		}
-		if (a[parent] > a[child])//大顶堆改>
+		Swap(&a[start], &a[mini]);
+		//第1轮在全范围查找时（此时start=0），如果最大值的下标为0，那么此时先交换a[start]和a[mini]，会使maxi指向的值变成了最小值，下面再执行Swap(&a[end], &a[maxi])时，会将最小值换到最后一位
+		//注意第i轮也是一样的（此时start=i），如果最大值的下标为i，那么此时先交换a[start]和a[mini]，会使maxi指向的值变成了最小值
+		//所以加一个条件判断，当这种情况出现时，及时将maxi指向mini，因为第i轮查找时（此时start=i），如果最大值的下标为i，a[start]和a[mini]交换后，a[mini]是最大值
+		if (maxi == start)
 		{
-			Swap(a + parent, a + child);
+			maxi = mini;
 		}
-		else
-		{
-			parent = child;
-			child = parent * 2 + 1;//堆排序的逻辑树是的物理结构是用数组存储的，这里的child与parent的等式使得向下调整函数可以进入左右子树重复执行，类比链式存储结构中用root->left,root->right去递归左右子树
-		}
+		Swap(&a[end], &a[maxi]);
+		start++;
+		end--;
 	}
 }
 
-/// <summary>
-/// 堆排序
-/// </summary>
-/// <param name="a">一个数组</param>
-/// <param name="b">该数组的大小</param>
-void HeapSort(int* a, int n)
+//冒泡排序
+void BubbleSort(int* a, int n)
 {
-	//第一步建堆
-	//向下调整的前提是左右子树都是小/大顶堆，所以堆排序就要先从最下面的最小的子树开始向上面执行	AdjustDown，这个过程叫建堆/堆化
-	for (int i = (n - 1 - 1) / 2; i >= 0; i--)//child = n-1; i = parent = (child - 1) / 2 ; 
+	for (int j = 0; j < n; ++j)
 	{
-		AdjustDown(a, n, i);
-	}
-
-	//第二步才是排序，怎样排序？取出堆顶元素，将堆顶元素与堆的最后一个元素交换。同时不破坏左右子树堆的结构
-	 
-	//排升序，要建大根堆
-	int end = n - 1;
-	while (end >= 1)//end最小是1，end=0时说明数组就剩一个元素了，不需要再比
-	{
-		Swap(&a[0], &a[end]);
-		AdjustDown(a, end, 0);
-		--end;
+		int exchange = 0;
+		for (int i = 1; i < n-j; ++i)
+		{
+			if (a[i] > a[i - 1])
+			{
+				Swap(&a[i], &a[i - 1]);
+				exchange = 1;
+			}
+		}
+		if (exchange == 0)
+		{
+			break;
+		}
 	}
 }
-
 
 void PrintArray(int* a, int n)
 {
@@ -70,13 +87,13 @@ void PrintArray(int* a, int n)
 void TestSort()
 {
 	int a[] = { 3,5,2,7,5,8,9,2,0,4,1 };
-	//InsertSort(a, sizeof(a) / sizeof(int));
+	SelectSort(a, sizeof(a) / sizeof(int));
 	PrintArray(a, sizeof(a) / sizeof(int));
 }
  
 int main()
 {
-	//TestSort();
+	TestSort();
 	//int a = 1, b = 2;
 	//Swap(&a, &b);
 	//printf("%d %d", a, b);
